da59471d0538b9437bdad3a92988a6ea7159c7a4
= <lightmorning46@gmail.com>
2017-12-13 13:52:22 +0800

    updating

--- index.php
+++ index.php
@@ -9,15 +9,20 @@
 		foreach($json_data as $key => $data)
 		{
 			$pages_sql = "INSERT INTO pages (page_id, theme_type,title) VALUES ({$key}, 0, '{$data['title']}')";
-			var_dump($pages_sql);
 			$mysqli->query($pages_sql);
 
 			$content = isset($data['tabs'][0]['top_header']) ? $mysqli->escape_string($data['tabs'][0]['top_header']) : '';
 
+
 			$topheader_sql = "INSERT INTO top_header (page_id, content) VALUES ({$key}, '$content')";
 			$mysqli->query($topheader_sql);
 
+			foreach($data['tabs'] as $tab_key => $each_tab)
+			{
+				$content = isset($each_tab['header']) ? $mysqli->escape_string($each_tab['header']) : '';
+				$header_sql = "INSERT INTO header (page_id, tab_id, content) VALUES ({$key}, {$tab_key}, '$content')";
+				$mysqli->query($header_sql);
+			}
 		}
-		// print_r($json_data);
 	}
 ?>
